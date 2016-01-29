5548bd489777411b4b31a5b128f785186f05af60
Paul <paullehenaff@msn.com>
2016-01-29 00:28:47 +0000

    Now has nice gallery effects. Also started adding more pieces. Might need a way of organising stuff better

--- mainapp/static/js/custom.js
+++ mainapp/static/js/custom.js
@@ -1,9 +1,29 @@
 // gallery thumbnail darkening effect
 $(".thumbnail").mouseenter( function (){
   console.log("mouse in")
-  $(".overlay").show();
-  $(this).find(".overlay").hide();
+  $(".dark-overlay").show();
+  $(this).find(".dark-overlay").hide();
+  $(this).find(".info-overlay").show();
+  timeoutObj.startT();
 }).mouseleave( function (){
   console.log("mouse out");
-  $(".overlay").hide();
+  $(".dark-overlay").hide();
+  $(".info-overlay").hide();
+  timeoutObj.clearT();
 });
+
+function TimeoutObj(){
+	var timeout = null;
+	return {
+	    startT: function(){
+			timeout = setTimeout(function() {
+		  		$(".dark-overlay").hide();
+		  	}, 1000);
+		},
+		clearT: function(){
+			clearTimeout(timeout);
+		}
+    }
+}
+
+var timeoutObj = TimeoutObj();
